55fc703f300f0cb782b9d4d8e717b62625d932ce
Sea Saingkoing <saingkoing.sea@rakuten.com>
2022-09-09 15:33:19 +0900

    no message

--- install-hook.sh
+++ install-hook.sh
@@ -1,5 +1,5 @@
 GIT_DIR=$(git rev-parse --git-dir)
 
 echo "Installing hooks..."
-ln -s scripts/pre-push.sh $GIT_DIR/hooks/pre-push
+ln -s ../../scripts/pre-push.sh $GIT_DIR/hooks/pre-push
 echo "Done"!
