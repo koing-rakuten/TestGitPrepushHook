5a82a315269ec0012dbf9a4266a21e7b30943043
Sea Saingkoing <saingkoing.sea@rakuten.com>
2022-09-09 15:23:30 +0900

    Init

---
+++ TestGitPrepushHook/ViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ViewModel.swift
+//  TestGitPrepushHook
+//
+//  Created by Sea, Saingkoing | MTSD on 2022/09/09.
+//
+
+import Foundation
+
+func sum(a: Int, b: Int) -> Int { a + b }
---
+++ scripts/pre-push.sh
@@ -0,0 +1,13 @@
+echo "Running pre-push hook"
+xcodebuild \
+  -workspace TestGitPrepushHook.xcworkspace \
+  -scheme TestGitPrepushHook \
+  -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone 13 Pro Max' \
+  test | xcpretty --test --color
+
+# $? stores exit value of the last command
+if [ $? -ne 0 ]; then
+    echo "Tests must be passed before pushing!"
+    exit 1
+fi
